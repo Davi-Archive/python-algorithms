7329a4a28736b9af7a6f4f05aadda1de13a75be4
d1av <davi4alves@gmail.com>
2022-11-24 18:44:47 -0300

    Initial commit algorithms

---
+++ python002/main.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    produtosDiponiveis = ["Laranja", "Suco", "Carne", "Refrigerante"]
+    produto = None
+    listaProdutos = []
+
+    while True:
+        produto = input('Digite o produto que deseja: (Digite 0 para finalizar)')
+
+        if produto == '0':
+            break
+
+        if produto in produtosDiponiveis:
+            listaProdutos.append(produto)
+        else:
+            print(f"O produto {produto} não está disponivel")
+
+        if not produto == '0':
+            listaProdutos.append(produto)
---
+++ pythonProject/main.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = 0
+
+    nome = input("Digite o seu nome")
+    idade = int(input("Digite a sua idade: "))
+
+if idade >= 18:
+    print(f'O(A) {nome} é maior de idade')
+else:
+    print(f'O(A) {nome} é menor de idade')
